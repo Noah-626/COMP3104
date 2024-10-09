97f62b0a0555a205cf24fc1e74be56c16ca4fedd
Noah-626 <ngnnhunam@gmail.com>
2024-10-09 19:57:30 -0400

    TITLE UPDATED

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101441905 || Nhu Nam Nguyen || DevOps</h5>
+      <h5>Developed By: 101441905 || Nhu Nam Nguyen || DevOps - COMP3104</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
